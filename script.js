2216955a4c4f58a230e9f600568de55d2bc96911
CGray1234 <cjgray1208@gmail.com>
2024-06-30 02:41:40 -0400

    fixed the cors issue

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const stormCount = document.getElementById('storm-count');
 
-    const apiUrl = 'https://www.nhc.noaa.gov/CurrentStorms.json';
+    const apiUrl = 'https://www.nhc.noaa.gov/CurrentStorms.json'
+    const proxyUrl = 'https://corsproxy.io/?';
 
-    fetch(apiUrl)
+    fetch(proxyUrl + apiUrl)
         .then(response => response.json())
         .then(data => {
-            const activeHurricanes = data.activeStorms.filter(storm => storm.stormType === 'HU');
-            const activeTropicalStorms = data.activeStorms.filter(storm => storm.stormType === 'TS');
+            const activeHurricanes = data.activeStorms.filter(storm => storm.classification === 'HU');
+            const activeTropicalStorms = data.activeStorms.filter(storm => storm.classification === 'TS');
             stormCount.textContent = `Found ${activeHurricanes.length} hurricanes and ${activeTropicalStorms.length} tropical storms.`;
         })
         .catch(error => {
