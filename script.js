cc1dfaae6ff655cb98649983635acd3dc7b76ff7
CGray1234 <cjgray1208@gmail.com>
2024-06-30 02:14:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const stormCount = document.getElementById('storm-count');
+
+    const apiUrl = 'https://www.nhc.noaa.gov/CurrentStorms.json';
+
+    fetch(apiUrl, { mode: "no-cors"})
+        .then(response => response.json())
+        .then(data => {
+            const activeHurricanes = data.activeStorms.filter(storm => storm.stormType === 'HU');
+            const activeTropicalStorms = data.activeStorms.filter(storm => storm.stormType === 'TS');
+            stormCount.textContent = `Found ${activeHurricanes.length} hurricanes and ${activeTropicalStorms.length} tropical storms.`;
+        })
+        .catch(error => {
+            console.error('Error fetching hurricane data:', error);
+            stormCount.textContent = 'Error loading data';
+        });
+});
