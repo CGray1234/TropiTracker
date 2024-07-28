fb9147a0f7e02ec0fc1cee6f9201ddd3b19d7832
CGray1234 <cjgray1208@gmail.com>
2024-07-28 00:15:32 -0400

    Add outlook text

--- scripts/outlooks.js
+++ scripts/outlooks.js
@@ -7,14 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const pacImage = document.getElementById("pacImage");
     const cpacImage = document.getElementById("cpacImage");
 
-    atlButton.onclick = () => changeImageDisplay(atlImage, pacImage, cpacImage);
-    pacButton.onclick = () => changeImageDisplay(pacImage, atlImage, cpacImage);
-    cpacButton.onclick = () => changeImageDisplay(cpacImage, pacImage, atlImage);
+    const atlUrl = `https://www.nhc.noaa.gov/xml/TWOAT.xml?timestamp=${new Date().getTime()}&date=${new Date().getDate()}`;
+    const epacUrl = `https://www.nhc.noaa.gov/xml/TWOEP.xml?timestamp=${new Date().getTime()}&date=${new Date().getDate()}`;
+    const pacUrl = `https://www.nhc.noaa.gov/xml/TWOCP.xml?timestamp=${new Date().getTime()}&date=${new Date().getDate()}`;
+    const proxyUrl = 'https://corsproxy.io/?';
+
+    const outlookText = document.getElementById("outlook");
+
+    fetchOutlookData(proxyUrl, atlUrl, outlookText);
+    
+    atlButton.onclick = () => changeImageDisplay(proxyUrl, atlUrl, outlookText, atlImage, pacImage, cpacImage);
+    pacButton.onclick = () => changeImageDisplay(proxyUrl, epacUrl, outlookText, pacImage, atlImage, cpacImage);
+    cpacButton.onclick = () => changeImageDisplay(proxyUrl, pacUrl, outlookText, cpacImage, pacImage, atlImage);
 });
 
-function changeImageDisplay(showImage, ...hideImages) {
+function fetchOutlookData(proxyUrl, rssUrl, outlookText) {
+    fetch(proxyUrl + rssUrl)
+        .then(response => response.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(data => {
+            const outlookData = data.getElementsByTagNameNS("*", "item")[0].getElementsByTagNameNS("*", "description")[0].textContent;
+
+            outlookText.innerHTML = outlookData;
+        })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function changeImageDisplay(proxyUrl, rssUrl, outlookText, showImage, ...hideImages) {
     showImage.style.display = "block";
     hideImages.forEach(image => image.style.display = "none");
+
+    fetchOutlookData(proxyUrl, rssUrl, outlookText);
 }
 
 function openNav() {
